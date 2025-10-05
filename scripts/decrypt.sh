308fdb5b67c29a4c59d2edfe2b211629815d1ef5
Rafaeltab <ikbenrafaelbieze2001@gmail.com>
2025-10-05 20:30:12 +0200

    Create everything for mimir in one go

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -3,3 +3,4 @@ cd cluster/secrets
 sops --decrypt --in-place godaddy_secret.yaml
 sops --decrypt --in-place root_credentials.yaml
 sops --decrypt --in-place loki_minio_user.yaml
+sops --decrypt --in-place mimir_minio_user.yaml
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -3,3 +3,4 @@ cd cluster/secrets
 sops --encrypt --in-place godaddy_secret.yaml
 sops --encrypt --in-place root_credentials.yaml
 sops --encrypt --in-place loki_minio_user.yaml
+sops --encrypt --in-place mimir_minio_user.yaml
